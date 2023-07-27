a86addd897fb19725ce6e550680ed0dc88343f82
disha <disshetty@deloitte.com>
2023-07-27 08:20:52 +0530

    yml file path changed

--- custom_policy_infracost.sh
+++ custom_policy_infracost.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 threshold_instance_type="large"
-instance_types=$(cat infracost-usage.yml | yq eval '.[] | select(.name | test("AWS EC2")) | .resources[].values.instance_type' -)
+instance_types=$(cat /home/runner/work/terraform/terraform/infracost_output.json | yq eval '.[] | select(.name | test("AWS EC2")) | .resources[].values.instance_type' -)
 
 if [[ "$instance_types" == *"$threshold_instance_type"* ]]; then
   echo "Error: Instances of type \"$threshold_instance_type\" found in the infrastructure. Policy violation."
@@ -10,7 +10,7 @@ fi
 
 
 threshold=3
-cost=$(cat infracost-usage.yml | yq eval '.[] | select(.name | test("AWS EC2")) | .cost_monthly.value' -)
+cost=$(cat /home/runner/work/terraform/terraform/infracost_output.json | yq eval '.[] | select(.name | test("AWS EC2")) | .cost_monthly.value' -)
 
 if (( $(echo "$cost > $threshold" | bc -l) )); then
   echo "Error: Estimated cost ($cost) exceeds the threshold of $threshold"
