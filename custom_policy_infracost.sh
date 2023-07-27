3709e8f095c80d29772c3e7696d3c20e69ebdd60
disha <disshetty@deloitte.com>
2023-07-27 11:27:53 +0530

    trail 11

--- custom_policy_infracost.sh
+++ custom_policy_infracost.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-# threshold_instance_type="large"
-# instance_types=$(cat infracost-usage.yml | yq eval '.[] | select(.name | test("AWS EC2")) | .resources[].values.instance_type' -)
+threshold_instance_type="large"
+instance_types=$(cat infracost_output.json | yq eval '.[] | select(.name | test("AWS EC2")) | .resources[].values.instance_type' -)
 
-# if [[ "$instance_types" == *"$threshold_instance_type"* ]]; then
-#   echo "Error: Instances of type \"$threshold_instance_type\" found in the infrastructure. Policy violation."
-#   exit 1
-# fi
+if [[ "$instance_types" == *"$threshold_instance_type"* ]]; then
+  echo "Error: Instances of type \"$threshold_instance_type\" found in the infrastructure. Policy violation."
+  exit 1
+fi
 
-# threshold=3
-# cost=$(cat infracost-usage.yml | yq eval '.[] | select(.name | test("AWS EC2")) | .monthlyCost.value' -)
+threshold=3
+cost=$(cat infracost_output.json | yq eval '.[] | select(.name | test("AWS EC2")) | .monthlyCost.value' -)
 
-# if (( $(echo "$cost > $threshold" | bc -l) )); then
-#   echo "Error: Estimated cost ($cost) exceeds the threshold of $threshold"
-#   exit 1
-# fi
-#!/bin/bash
-
-# Use jq to extract the totalMonthlyCost value from the JSON file
-totalMonthlyCost=$(jq -r '.totalMonthlyCost' infracost_output.json)
-
-# Print the value without any additional text
-echo "$totalMonthlyCost"
+if (( $(echo "$cost > $threshold" | bc -l) )); then
+  echo "Error: Estimated cost ($cost) exceeds the threshold of $threshold"
+  exit 1
+fi
