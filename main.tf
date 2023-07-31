7e36a06db4245c5072630daef6825318fe886b69
disha <disshetty@deloitte.com>
2023-07-31 12:57:47 +0530

    .hcl added trail8

--- main.tf
+++ main.tf
@@ -116,7 +116,7 @@ provider "aws" {
   region = "us-west-2"  # Change this to your desired AWS region
 }
 
-resource "aws_s3_bucket" "example_bucket" {
+resource "aws_s3_bucket" "my-company-disha-data" {
   bucket = "my-company-disha-data"  # Change this to a unique bucket name
 
   acl    = "private"  # Access control list for the bucket
