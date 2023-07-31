5c068fbb74361317fc153d8e362285fda477e3df
disha <disshetty@deloitte.com>
2023-07-31 12:48:04 +0530

    .hcl added trail7

--- custom_policy.sh
+++ custom_policy.sh
@@ -1,5 +1,15 @@
-custom {
-  name     = "s3_bucket_naming"
-  message  = "S3 bucket name must follow the naming convention: my-company-{name}-data"
-  argument = "not regex(\"^my-company-[a-z0-9.-]{3,63}-data$\", var.resource_name)"
-}
+#!/bin/bash
+
+# Enforce naming convention for S3 buckets
+# Bucket name must start with "my-company-" and end with "-data"
+
+# Get the list of S3 bucket names from Terraform files
+bucket_names=$(grep -E -o 'resource "aws_s3_bucket" "[^"]+"' *.tf | awk '{print $3}' | sed 's/"//g')
+
+# Check each bucket name against the naming convention
+for name in $bucket_names; do
+  if ! echo "$name" | grep -qE '^my-company-[a-z0-9.-]{3,63}-data$'; then
+    echo "S3 bucket name does not follow the naming convention: my-company-{name}-data"
+    exit 1
+  fi
+done
