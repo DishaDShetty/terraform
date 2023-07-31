6397814aa546d0f2a37da2a2f62144d3877a7af3
disha <disshetty@deloitte.com>
2023-07-31 14:44:28 +0530

    .sh added and tested

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "my-company-disha-data" {
-  bucket = "my-company-disha-data"  # Change this to a unique bucket name
+  bucket = "my-com"  # Change this to a unique bucket name
 
   acl    = "private"  # Access control list for the bucket
 
