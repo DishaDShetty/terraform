e472d6c420b5bfa82621f5f1a8ef70d9b8208d80
disha <disshetty@deloitte.com>
2023-08-01 17:39:09 +0530

    check for both execution

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   region = "us-west-2"  # Change this to your desired AWS region
 }
 
-resource "aws_s3_bucket" "my-co" {
+resource "aws_s3_bucket" "my-company-disha-data" {
   bucket = "my-company-disha-data"  # Change this to a unique bucket name
 
   acl    = "private"  # Access control list for the bucket
