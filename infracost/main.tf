4fc91a882e5f9b296463d6ff136d9bae98a2adba
disha <disshetty@deloitte.com>
2023-08-01 10:41:23 +0530

    final for infracost

--- infracost/main.tf
+++ infracost/main.tf
@@ -1,21 +1,15 @@
 provider "aws" {
-  alias  = "apprunner"
-  region = "us-west-2"  # Replace this with the desired AWS region for App Runner
-
-  # You can set different AWS credentials for App Runner here, if needed
-  # access_key = "your_access_key"
-  # secret_key = "your_secret_key"
+  region = "us-west-2"  # Replace this with your desired AWS region
 }
-resource "aws_apprunner_service" "example_app_runner" {
-  provider = aws.apprunner  # Use the aliased provider
 
-  name = "example-apprunner-service"
+resource "aws_apprunner_service" "example_app_runner" {
+  name        = "example-apprunner-service"
   repository {
     # Replace the following with the details of your container image
     image_repository_type = "ECR"  # Use "ECR" if you are using Amazon ECR, "ECR_PUBLIC" for public images, or "ECR_PRIVATE" for private images.
     image_uri             = "your-ecr-repo-uri:latest"  # Replace with your container image URI
   }
-
+  
   # Optionally, you can define a custom domain mapping for the App Runner service
   custom_domain {
     domain_name = "example.domain.com"
