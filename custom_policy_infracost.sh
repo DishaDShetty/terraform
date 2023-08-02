0f94a5e6c546e2f26e21988b75e6e43f9f587c89
disha <disshetty@deloitte.com>
2023-08-02 18:40:38 +0530

    infra cost customised trail 10 again///

--- custom_policy_infracost.sh
+++ custom_policy_infracost.sh
@@ -18,8 +18,8 @@ threshold=3
 # Use jq to parse JSON output and extract the totalMonthlyCost value for AWS EC2 instances
 #cat /home/runner/work/terraform/terraform/infracost_output.json
 #ec2Cost=$(jq '.projects[0].pastBreakdown.resources[] | select(.resourceType == "aws_instance") | .monthlyCost' infracost_output.json )
-name_Cost=$(jq -r '.projects[0].pastBreakdown.resources[] | "\(.name): \(.monthlyCost)"' infracost_output.json)
-echo $name_Cost
+echo $(jq -r '.projects[0].pastBreakdown.resources[] | "\(.name): \(.monthlyCost)"' infracost_output.json)
+
 # if (( $(echo "$ec2Cost > $threshold" | bc -l) )); then
 #   echo "Error: Estimated cost ($cost) exceeds the threshold of $threshold"
 #   exit 1
