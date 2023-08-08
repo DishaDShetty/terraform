7a05c6c0c9a21b9c1961e762d3c409ca820413da
disha <disshetty@deloitte.com>
2023-08-08 10:21:27 +0530

    random

--- custom_policy_infracost.sh
+++ custom_policy_infracost.sh
@@ -12,12 +12,19 @@ threshold=3
 # fi
 # Use jq to parse JSON output and extract the totalMonthlyCost value for AWS EC2 instances
 #cat /home/runner/work/terraform/terraform/infracost_output.json
-ec2Cost=$(jq -r '.projects[0].pastBreakdown.resources[] | .monthlyCost' infracost_output.json )
+#ec2Cost=$(jq -r '.projects[0].pastBreakdown.resources[] | .monthlyCost' infracost_output.json )
 
-#ec2Cost=$(jq -r '.projects[0].pastBreakdown.resources[] | select(.resourceType == "aws_instance") | .monthlyCost' infracost_output.json )
+ec2Cost=$(jq -r '.projects[0].pastBreakdown.resources[] | select(.resourceType == "aws_instance") | .monthlyCost' infracost_output.json )
+s3Cost=$(jq -r '.projects[0].pastBreakdown.resources[] | select(.resourceType == "aws_s3_bucket") | .monthlyCost' infracost_output.json )
+
 echo $ec2Cost
 if (( $(echo "$ec2Cost > $threshold" | bc -l) )); then
   echo "Error: Estimated EC2 cost ($ec2Cost) exceeds the threshold of $threshold"
+  exit 1
+fi
+echo $s3Cost
+if (( $(echo "$s3Cost > $threshold" | bc -l) )); then
+  echo "Error: Estimated EC2 cost ($s3Cost) exceeds the threshold of $threshold"
   exit 1
 fi
 #$(jq -r '.projects[0].pastBreakdown.resources[] | "\(.name): \(.monthlyCost)"' infracost_output.json)
