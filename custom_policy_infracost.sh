5235f16d3eb64f94f9615486ab7d237a4dff7b0b
disha <disshetty@deloitte.com>
2023-08-08 08:30:19 +0530

    same thes for all resources trail6

--- custom_policy_infracost.sh
+++ custom_policy_infracost.sh
@@ -12,7 +12,7 @@ threshold=3
 # fi
 # Use jq to parse JSON output and extract the totalMonthlyCost value for AWS EC2 instances
 #cat /home/runner/work/terraform/terraform/infracost_output.json
-ec2Cost=$(jq -r '.projects[0].pastBreakdown.resources[] | .resourceType[] | .monthlyCost' infracost_output.json )
+ec2Cost=$(jq -r '.projects[0].pastBreakdown.resources[] | .monthlyCost' infracost_output.json )
 
 #ec2Cost=$(jq -r '.projects[0].pastBreakdown.resources[] | select(.resourceType == "aws_instance") | .monthlyCost' infracost_output.json )
 echo $ec2Cost
